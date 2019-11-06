a7d43a1f8adbaead15a3328d36091c72c5906987
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-06 16:21:46 +0530

    square root added

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -689,6 +689,14 @@ const operators = function () {
                 </shadow>
             </value>
         </block>
+        ${blockSeparator}
+        <block type="operator_square_root">
+            <value name="NUM">
+                <shadow type="math_number">
+                    <field name="NUM"/>
+                </shadow>
+            </value>
+        </block>
         ${categorySeparator}
     </category>
     `;
